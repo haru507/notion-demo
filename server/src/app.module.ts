239c6fe2a254e3195fdce013e6f07936d17cdc10
haru507 <i.haru507@gmail.com>
2022-02-26 16:04:14 +0900

    不要なファイルの削除

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,16 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-import { TodoModule } from './todo/todo.module';
-
-import { APP_FILTER, APP_PIPE } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), TodoModule, AuthModule, UsersModule],
+  imports: [TypeOrmModule.forRoot()],
   controllers: [AppController],
   providers: [
   ],
